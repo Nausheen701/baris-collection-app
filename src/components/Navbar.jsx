5fe9d2a92e33efd7aedba80fc35fbfc8988a8c56
Nausheen Akhter <nausheen701@gmail.com>
2022-06-06 12:39:14 +0300

    navbar padding

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,45 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const  Container  = styled.div`
-height:  60px;
-background-color: black;
+const Container  = styled.div`
+    height:  60px;
+    
+    // background-color: black
+`;
+
+const Wrapper = styled.div`
+    padding: 10px  20px;
+    display: flex;
+    justify-content: space-between;
+`;
+
+const Left = styled.div`
+    flex: 1;
+// width:  33.3%;
+`
+
+
+const Center = styled.div`
+    flex: 1;
+// width:  33.3%;
+`
+const Right = styled.div`
+    flex: 1;
+// width:  33.3%;
 `
 
 const Navbar = () => {
   return (
-    <Container>
-      navbar
-    </Container>
+      <Container>
+         <Wrapper> 
+             <Left>left</Left>
+             <Center>center</Center>
+             <Right>right</Right>
+         </Wrapper>
+      </Container>
+    // <div className="container">
+    //   navbar
+    // </div>
   )
 }
 
