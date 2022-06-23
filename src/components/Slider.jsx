b9c6b251b9b87a43bec20dd10b325dd74a25b101
Nausheen Akhter <nausheen701@gmail.com>
2022-06-23 14:16:56 -0400

    slider container

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,1 +1,20 @@
 
+import styled from 'styled-components'
+
+const Container = styled.div `
+    width: 100%;
+    height: 100vh;
+    display: flex;
+`
+
+const Slider = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Slider
+
+
