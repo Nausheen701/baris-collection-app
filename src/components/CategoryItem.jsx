14f8755e25585d1b384e04e190956f5c56b666a6
Nausheen Akhter <nausheen701@gmail.com>
2022-08-22 19:15:16 -0400

    categ info css

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -13,6 +13,15 @@ const Image = styled.img`
     object-width: cover;
     `;
 
+const Info = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0; 
+    width: 100%;
+    height: 100%;
+    display: flex;
+    `;
+
 const CategoryItem = () => {
   return (
     <Container>
