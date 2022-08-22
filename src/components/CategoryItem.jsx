6cf4445f72320215f96f4dbe154e8172deceb0af
Nausheen Akhter <nausheen701@gmail.com>
2022-08-22 19:14:52 -0400

    categ image css

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -7,6 +7,12 @@ const Container = styled.div`
     position: relative;
 `;
 
+const Image = styled.img`
+    width: 100%;
+    height: 100%;
+    object-width: cover;
+    `;
+
 const CategoryItem = () => {
   return (
     <Container>
