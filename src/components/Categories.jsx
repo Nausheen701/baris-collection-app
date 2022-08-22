7e5011b154035644fc6b772976b05f5c198b5799
Nausheen Akhter <nausheen701@gmail.com>
2022-08-22 19:06:58 -0400

    categories styling

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,6 +1,13 @@
 import styled from "styled-components"
 import { categories } from "../data"
 
+const Container = styled.div`
+    display: flex;
+    padding: 20px;
+    justfy-content: space-between;
+    `
+
+
 const Categories = () => {
   return (
     
