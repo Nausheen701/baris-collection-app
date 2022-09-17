9239c357ab2bd679c5bcb236dbc3327a6efd9630
Nausheen Akhter <nausheen701@gmail.com>
2022-09-17 13:52:29 +0300

    products

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -26,10 +26,18 @@ const Info = styled.div`
     `;
 
 const Title = styled.h1`
-   
+    color: white;
+    margin-bottom: 20px;
 
 `;
-const Button = styled.button``;
+const Button = styled.button`
+    border: none;
+    padding: 10px;
+    background-color: white;
+    color: gray;
+    cursor: pointer;
+    font-weight: 600;
+    `;
 
 const CategoryItem = ({item}) => {
   return (
