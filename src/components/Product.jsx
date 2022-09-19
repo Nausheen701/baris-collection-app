6df28b20363ebc710e9b3ef3e1d77c739964d4bf
Nausheen Akhter <nausheen701@gmail.com>
2022-09-19 11:34:59 +0300

    product icons

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,11 +1,27 @@
 import styled from "styled-components"
 
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+
 const Container = styled.div``
 
-const Product = () => {
+const Product = ({item}) => {
   return (
    <Container> 
-
+        <Circle/>
+        <Image src={item.img}/>
+        <Info>
+            <Icon>
+            <ShoppingCartOutlinedIcon />
+            </Icon>
+            <Icon>
+            <SearchOutlinedIcon />
+            </Icon>
+            <Icon>
+            <FavoriteBorderOutlinedIcon />
+            </Icon>
+        </Info>
 
    </Container> 
   )
