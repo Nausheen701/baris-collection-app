08c781301b6c495e7cf0bacff29b7203bb0df5e0
Nausheen Akhter <nausheen701@gmail.com>
2022-09-24 14:44:09 +0300

    prod style

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -9,10 +9,19 @@ const Container = styled.div`
     margin: 5px;
     min-width: 280px;
     height: 350px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #f5fbfd; 
 `
 
-const Circle = styled.div``
-const Image = styled.img``
+const Circle = styled.div`
+    width: 22px;
+    height: 200px;
+`
+const Image = styled.img`
+    height: 75%
+    `
 const Info = styled.div``
 const Icon = styled.div``
 
