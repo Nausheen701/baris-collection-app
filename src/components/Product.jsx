29349d1361f17d5ea712d6515b1aa3cbd23e4f4c
Nausheen Akhter <nausheen701@gmail.com>
2022-09-24 15:18:44 +0300

    prod info style

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -5,7 +5,7 @@ import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 
 const Container = styled.div`
-    flex:1;
+    flex: 1;
     margin: 5px;
     min-width: 280px;
     height: 350px;
@@ -27,20 +27,32 @@ const Image = styled.img`
     z-index: 2;
     `
 const Info = styled.div`
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    top: 0;
-    left: 0;
-    background-color: gray;
-    z-index: 3;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+// width: 100%;
+// height: 100%;
+// position: absolute;
+// top: 0;
+// left: 0;
+// background-color: gray;
+// z-index: 3;
+// display: flex;
+// align-items: center;
+// justify-content: center;
 `
 
 const Icon = styled.div`
-  
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 10px;
+  transition: all 0.5s ease;
+  &:hover{
+    background-color: #e9f5f5;
+    transform: scale(1.1);
+  }
 `
 
 const Product = ({item}) => {
