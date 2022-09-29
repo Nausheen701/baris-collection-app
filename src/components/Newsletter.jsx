fcf72aa1c585b610a845aa4ed508df3226a77979
Nausheen Akhter <nausheen701@gmail.com>
2022-09-29 13:12:25 +0300

    button css

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -18,7 +18,7 @@ const Title = styled.h1`
 const Description = styled.div`
     font-size: 24px;
     font-weight: 300;
-    margin-bottom: 
+    margin-bottom: 20px;
     `
 const InputContainer = styled.div`
     width: 50%;
@@ -29,8 +29,18 @@ const InputContainer = styled.div`
     border: 1px solid lightgray;
 `
 
-const Input = styled.input``
-const Button = styled.button``
+const Input = styled.input`
+    border: none;
+    flex: 8;
+    padding-left: 20px;
+    `
+
+const Button = styled.button`
+    flex: 1;
+    border: none;
+    background-color: teal;
+    font-color: white;
+`
 
 
 const Newsletter = () => {
