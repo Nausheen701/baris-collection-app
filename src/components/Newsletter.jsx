ecfe6f424b1c837b422feb8db92fc5e843a4b83e
Nausheen Akhter <nausheen701@gmail.com>
2022-09-29 12:51:26 +0300

    divs

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -2,7 +2,11 @@ import SendOutlinedIcon from '@mui/icons-material/SendOutlined';
 import styled from 'styled-components'
 
 const Container = styled.div``
-
+const Title = styled.h1``
+const Description = styled.div``
+const InputContainer = styled.div``
+const Input = styled.input``
+const Button = styled.button``
 
 
 const Newsletter = () => {
