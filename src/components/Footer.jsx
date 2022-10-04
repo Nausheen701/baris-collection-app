93622b25d6001e3ee57d0a5a2b946ba045cd0869
Nausheen Akhter <nausheen701@gmail.com>
2022-10-04 13:16:07 +0300

    social container css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,28 @@
 import styled from "styled-components"
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined'
+import InstagramIcon from '@mui/icons-material/Instagram'
 
 const Container = styled.div`
     display: flex;
     `
 const Left = styled.div`
     flex: 1;
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
     `
+const Logo = styled.h1``
+const Desc = styled.p`
+    margin: 2px 0px;
+    `
+const SocialContainer = styled.div`
+    display: flex;
+    `
+const SocialIcon = styled.div`
+    flex-direction: row;
+    `
+
+
 const Center = styled.div`
     flex: 1;`
 
@@ -22,17 +39,15 @@ const Footer = () => {
             Specializing in the season's latest bags, trendy sunglasses, and chic watches.
             </Desc>
         </Left>
-        <SocialContainer>
-            
+        
+        <SocialContainer>    
             <SocialIcon>
-                <Instagram/>
+                <InstagramIcon/>
             </SocialIcon>
 
             <SocialIcon>
-                <Facebook/>
+            <FacebookOutlinedIcon />
             </SocialIcon>
-
-
         </SocialContainer>
 
         <Center>
